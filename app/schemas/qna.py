3cdf187372378fb639d53a386df00eca9ab52d52
Mohamad Ali Jaafar <mohamadali.jaafar@montyholding.com>
2025-10-10 13:55:05 +0300

    feat: Introduced the API that performs the QnA summary `/qna_summary`

--- app/schemas/qna.py
+++ app/schemas/qna.py
@@ -3,7 +3,6 @@ from pydantic import BaseModel, Field
 
 class QnARequest(BaseModel):
     """Request wrapper for analysis"""
-    # example: "my-project"
     repo_alias_name: str = Field(
         ...,
         description="The repository alias name",
